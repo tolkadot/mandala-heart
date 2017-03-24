570fd376d00c71a8542f2c50a12a78412696e661
tolkadot <tolkadot@gmail.com>
2017-03-24 03:01:28 +0000

    header changes

--- front-page.php
+++ front-page.php
@@ -16,21 +16,27 @@ get_header(); ?>
 
 <div id="primary" class="content-area">
 	<main id="main" class="site-main" role="main">
+		<div class="wrap">
 
 		<?php 
 		$mandalaHeartImage = get_field("mandala-heart-image");
 		$mandalaHeartText = get_field("mandala-heart-text");
+		$mandalasTitle = get_field("mandala-heart-title");
 		$aboutAhnnaImage = get_field("about-ahnna-image");
 		$aboutAhnnaText = get_field("about-ahnna-text");
+		$ahnna  = get_field("about-ahnna-title");
 		$mandalasImage = get_field("mandalas-image");
 		$mandalasText = get_field("mandalas-text");
+		$mandalas = get_field("mandalas");
+		
 		 
 		?>
 		<div class=container>
 		<section class ="front-page-section" id="mandala-heart" >
 			<div class="front-page-container">
 				<div class="mandala-heart-text-area front-page-text-area">
-					<?php echo $mandalaHeartText;?>
+					<h3><?php echo $mandalasTitle; ?></h3>
+					<?php echo $mandalaHeartText; ?>
 				</div>
 				<div class="mandala-heart-image-area front-page-image-area">
 					<img id="mandala-heart-image" src="<?php echo $mandalaHeartImage['url']; ?> "/>
@@ -39,19 +45,21 @@ get_header(); ?>
 		</section>
 		
 		<section class ="front-page-section" id="about-ahnna">
-			<div class="front-page-container">
+			<div class="front-page-container front-page-container-ahnna">
+				<div class="mandala-heart-text-area front-page-text-area">
+						<h3><?php echo $ahnna; ?></h3>
+					<?php echo $aboutAhnnaText; ?>
+				</div>
 				<div class="mandala-heart-image-area front-page-image-area">
 					<img id="ahnna-profile-pic" src="<?php echo $aboutAhnnaImage['url']; ?> "/>
 				</div>
-				<div class="mandala-heart-text-area front-page-text-area">
-					<?php echo $aboutAhnnaText; ?>
-				</div>	
 			</div>
 		</section>
 		
 		<section class ="front-page-section" id="mandalas">	
 		<div class="front-page-container front-page-container-end">
 				<div class="mandala-heart-text-area front-page-text-area">
+						<h3><?php echo $mandalas; ?></h3>
 					<?php echo $mandalasText; ?>
 				</div>
 				<div class="mandala-heart-image-area front-page-image-area">
@@ -60,7 +68,7 @@ get_header(); ?>
 			</div>
 		</section>
 		</div>
-		
+		</div><!-- #wrap-->
 	</main><!-- #main -->
 </div><!-- #primary -->
 
